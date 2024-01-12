86951a654bfe20fb3f0d51514eda2900972bf662
Jay <152004102+jayvsc@users.noreply.github.com>
2024-01-12 22:51:50 +0100

    Upl
    Ukoad
    
    Co-Authored-By: tjf1 <117757363+tomekjestfajny1@users.noreply.github.com>

---
+++ win.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+@rem Change title
+title MS-DOS Mode
+
+@rem Execute Powershell script
+@rem NOTE TO USER: You can modify the path at the end,
+@rem but recommended to leave as is
+powershell.exe -executionpolicy remotesigned -File %systemroot%\win.ps1
